e4f9c405f1de20a6a5cf99f7afd5f0e737256920
Daniel Moore <sttyerase@gmail.com>
2020-05-05 23:11:34 -0400

    Import database design documents.

---
+++ design/CreateSeedInspectionDB.sql
@@ -0,0 +1,241 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema SeedInspectionDB
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `SeedInspectionDB` ;
+
+-- -----------------------------------------------------
+-- Schema SeedInspectionDB
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `SeedInspectionDB` ;
+USE `SeedInspectionDB` ;
+
+-- -----------------------------------------------------
+-- Table `SeedInspectionDB`.`producers`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `SeedInspectionDB`.`producers` ;
+
+CREATE TABLE IF NOT EXISTS `SeedInspectionDB`.`producers` (
+  `producer_id` INT NOT NULL,
+  `producer_name` VARCHAR(200) NULL,
+  `producer_address1` VARCHAR(200) NULL,
+  `producer_address2` VARCHAR(200) NULL,
+  `producer_city` VARCHAR(200) NULL,
+  `producer_state` VARCHAR(2) NULL,
+  `producer_zip` VARCHAR(10) NULL,
+  PRIMARY KEY (`producer_id`))
+ENGINE = InnoDB;
+
+CREATE UNIQUE INDEX `producer_name_UNIQUE` ON `SeedInspectionDB`.`producers` (`producer_name` ASC) VISIBLE;
+
+
+-- -----------------------------------------------------
+-- Table `SeedInspectionDB`.`crops`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `SeedInspectionDB`.`crops` ;
+
+CREATE TABLE IF NOT EXISTS `SeedInspectionDB`.`crops` (
+  `crop_id` BIGINT NOT NULL AUTO_INCREMENT,
+  `crop_name` VARCHAR(200) NULL,
+  `crop_description` VARCHAR(2000) NULL,
+  `crop_icc_code` INT NULL,
+  PRIMARY KEY (`crop_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = ascii;
+
+CREATE UNIQUE INDEX `crop_name_UNIQUE` ON `SeedInspectionDB`.`crops` (`crop_name` ASC) VISIBLE;
+
+
+-- -----------------------------------------------------
+-- Table `SeedInspectionDB`.`varieties`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `SeedInspectionDB`.`varieties` ;
+
+CREATE TABLE IF NOT EXISTS `SeedInspectionDB`.`varieties` (
+  `variety_id` INT NOT NULL AUTO_INCREMENT,
+  `variety_name` VARCHAR(100) NOT NULL,
+  `variety_crop_id` BIGINT NOT NULL,
+  `variety_description` VARCHAR(2000) NULL,
+  PRIMARY KEY (`variety_id`),
+  CONSTRAINT `fk_varieties_crops1`
+    FOREIGN KEY (`variety_crop_id`)
+    REFERENCES `SeedInspectionDB`.`crops` (`crop_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE INDEX `fk_varieties_crops1_idx` ON `SeedInspectionDB`.`varieties` (`variety_crop_id` ASC) VISIBLE;
+
+
+-- -----------------------------------------------------
+-- Table `SeedInspectionDB`.`samples`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `SeedInspectionDB`.`samples` ;
+
+CREATE TABLE IF NOT EXISTS `SeedInspectionDB`.`samples` (
+  `sample_id` INT NOT NULL,
+  `sample_crop_id` INT NOT NULL,
+  `sample_entry_date` DATE NOT NULL,
+  `sample_producer_id` INT NOT NULL,
+  `sample_variety_id` INT NOT NULL,
+  `sample_lot_id` VARCHAR(45) NULL,
+  PRIMARY KEY (`sample_id`, `sample_variety_id`),
+  CONSTRAINT `fk_samples_producers`
+    FOREIGN KEY (`sample_producer_id`)
+    REFERENCES `SeedInspectionDB`.`producers` (`producer_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_samples_varieties`
+    FOREIGN KEY (`sample_variety_id`)
+    REFERENCES `SeedInspectionDB`.`varieties` (`variety_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE INDEX `fk_samples_producers1_idx` ON `SeedInspectionDB`.`samples` (`sample_producer_id` ASC) VISIBLE;
+
+CREATE INDEX `fk_samples_varieties1_idx` ON `SeedInspectionDB`.`samples` (`sample_variety_id` ASC) VISIBLE;
+
+
+-- -----------------------------------------------------
+-- Table `SeedInspectionDB`.`germinations`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `SeedInspectionDB`.`germinations` ;
+
+CREATE TABLE IF NOT EXISTS `SeedInspectionDB`.`germinations` (
+  `germination_id` INT NOT NULL COMMENT 'germinated + weak + dead = 100\n',
+  `percent_germinated` DOUBLE NOT NULL DEFAULT 0,
+  `percent_dead` DOUBLE NOT NULL DEFAULT 0,
+  `percent_weak` DOUBLE NOT NULL DEFAULT 0,
+  `germination_comments` VARCHAR(2000) NULL,
+  `germination_sample_id` INT NOT NULL,
+  PRIMARY KEY (`germination_id`),
+  CONSTRAINT `fk_germinations_samples`
+    FOREIGN KEY (`germination_sample_id`)
+    REFERENCES `SeedInspectionDB`.`samples` (`sample_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE INDEX `fk_germinations_samples1_idx` ON `SeedInspectionDB`.`germinations` (`germination_sample_id` ASC) VISIBLE;
+
+
+-- -----------------------------------------------------
+-- Table `SeedInspectionDB`.`purities`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `SeedInspectionDB`.`purities` ;
+
+CREATE TABLE IF NOT EXISTS `SeedInspectionDB`.`purities` (
+  `purity_id` INT NOT NULL,
+  `purity_sample_id` INT NULL,
+  `pure_seed` DOUBLE ZEROFILL NOT NULL,
+  `foreign_matter` DOUBLE ZEROFILL NOT NULL,
+  PRIMARY KEY (`purity_id`),
+  CONSTRAINT `fk_purities_samples1`
+    FOREIGN KEY (`purity_sample_id`)
+    REFERENCES `SeedInspectionDB`.`samples` (`sample_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+COMMENT = 'purity results for sample.';
+
+CREATE INDEX `fk_purities_samples1_idx` ON `SeedInspectionDB`.`purities` (`purity_sample_id` ASC) VISIBLE;
+
+
+-- -----------------------------------------------------
+-- Table `SeedInspectionDB`.`producer_privacy_keys`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `SeedInspectionDB`.`producer_privacy_keys` ;
+
+CREATE TABLE IF NOT EXISTS `SeedInspectionDB`.`producer_privacy_keys` (
+  `producer_pkkeys_id` INT NOT NULL,
+  `producer_pkkeys_key` VARCHAR(2048) NOT NULL,
+  `producer_pkkeys_producer_id` INT NOT NULL,
+  PRIMARY KEY (`producer_pkkeys_id`),
+  CONSTRAINT `fk_producer_pkkeys_producers1`
+    FOREIGN KEY (`producer_pkkeys_producer_id`)
+    REFERENCES `SeedInspectionDB`.`producers` (`producer_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+COMMENT = 'Producer-privacy key is a unique encrypted key, registered at the time of the account creation.  The producer-privacy key is then used to produce a unique\ntoken upon entry to the system.  Each token expires at the end of the transaction.   The producer privacy key also identifies which records the producer may access.\n\n';
+
+CREATE INDEX `fk_producer_pkkeys_producers1_idx` ON `SeedInspectionDB`.`producer_privacy_keys` (`producer_pkkeys_producer_id` ASC) VISIBLE;
+
+
+-- -----------------------------------------------------
+-- Table `SeedInspectionDB`.`fields`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `SeedInspectionDB`.`fields` ;
+
+CREATE TABLE IF NOT EXISTS `SeedInspectionDB`.`fields` (
+  `field_id` INT NOT NULL,
+  `field_location` VARCHAR(1000) NULL,
+  `field_producer_id` INT NOT NULL,
+  `field_variety_id` INT NOT NULL,
+  `field_inspection_date` DATE NULL,
+  `field_inspection_results` VARCHAR(100) NULL,
+  `field_inspection_comments` VARCHAR(2000) NULL,
+  `field_application_date` DATE NOT NULL,
+  PRIMARY KEY (`field_id`),
+  CONSTRAINT `fk_fields_producers1`
+    FOREIGN KEY (`field_producer_id`)
+    REFERENCES `SeedInspectionDB`.`producers` (`producer_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_fields_varieties1`
+    FOREIGN KEY (`field_variety_id`)
+    REFERENCES `SeedInspectionDB`.`varieties` (`variety_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE INDEX `fk_fields_producers1_idx` ON `SeedInspectionDB`.`fields` (`field_producer_id` ASC) VISIBLE;
+
+CREATE INDEX `fk_fields_varieties1_idx` ON `SeedInspectionDB`.`fields` (`field_variety_id` ASC) VISIBLE;
+
+
+-- -----------------------------------------------------
+-- Table `SeedInspectionDB`.`fieldinspections`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `SeedInspectionDB`.`fieldinspections` ;
+
+CREATE TABLE IF NOT EXISTS `SeedInspectionDB`.`fieldinspections` (
+  `fieldinspection_id` INT NOT NULL AUTO_INCREMENT,
+  `fieldinspection_pass` VARCHAR(10) NULL,
+  `field_inspection_results` VARCHAR(500) NULL,
+  `fieldinspection_field_id` INT NOT NULL,
+  `fieldinspection_application_number` INT NOT NULL,
+  PRIMARY KEY (`fieldinspection_id`),
+  CONSTRAINT `fk_fieldinspections_fields`
+    FOREIGN KEY (`fieldinspection_field_id`)
+    REFERENCES `SeedInspectionDB`.`fields` (`field_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE INDEX `fk_fieldinspections_fields_idx` ON `SeedInspectionDB`.`fieldinspections` (`fieldinspection_field_id` ASC) VISIBLE;
+
+SET SQL_MODE = '';
+DROP USER IF EXISTS seedmgr;
+SET SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+CREATE USER 'seedmgr' IDENTIFIED BY 'daLLman8';
+
+GRANT ALL ON `SeedInspectionDB`.* TO 'seedmgr';
+GRANT ALL ON TABLE `SeedInspectionDB`.`fields` TO 'seedmgr';
+GRANT ALL ON TABLE `SeedInspectionDB`.`crops` TO 'seedmgr';
+GRANT ALL ON TABLE `SeedInspectionDB`.`germinations` TO 'seedmgr';
+GRANT ALL ON TABLE `SeedInspectionDB`.`producer_privacy_keys` TO 'seedmgr';
+GRANT ALL ON TABLE `SeedInspectionDB`.`producers` TO 'seedmgr';
+GRANT ALL ON TABLE `SeedInspectionDB`.`purities` TO 'seedmgr';
+GRANT ALL ON TABLE `SeedInspectionDB`.`samples` TO 'seedmgr';
+GRANT ALL ON TABLE `SeedInspectionDB`.`varieties` TO 'seedmgr';
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
