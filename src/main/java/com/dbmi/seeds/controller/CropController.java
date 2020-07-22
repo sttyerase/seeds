e4c6ffc8add3c756ed56fc6130f0b37e97f2ebb9
Daniel Moore <sttyerase@gmail.com>
2020-07-22 11:44:55 -0400

    Minor refactoring: method names and locations.

--- src/main/java/com/dbmi/seeds/controller/CropController.java
+++ src/main/java/com/dbmi/seeds/controller/CropController.java
@@ -19,27 +19,39 @@ public class CropController {
     @Autowired
     private CropRepository cropRepository;
 
+    // GET METHODS
     @GetMapping("/crops/all")
-    public Iterable<Crop> getAllCrops() {
+    public Iterable<Crop> findAllCrops() {
         return cropRepository.findAll();
     } // GETALLCROPS()
 
-    @GetMapping("/crops/{cropId}")
+    @GetMapping("/crops/id/{cropId}")
     public ResponseEntity<Crop> getCropsById(@PathVariable(value = "cropId") Long cropId)
             throws ResourceNotFoundException {
         Crop myCrop =
                 cropRepository
                         .findById(cropId)
                         .orElseThrow(() -> new ResourceNotFoundException("Crop information not found for id: " + cropId));
-        ResponseEntity<Crop> theRE = new ResponseEntity<Crop>(myCrop,HttpStatus.OK);
-        return theRE;
+        return new ResponseEntity<Crop>(myCrop,HttpStatus.OK);
     } // GETCROPSBYID(LONG)
 
+    @GetMapping("/")
+    public ResponseEntity<String> getHome() throws Exception {
+        return ResponseEntity.ok("We are at home page.");
+    } // GETHOME()
+
+    @GetMapping("/error")
+    public ResponseEntity<String> getError() throws Exception {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("500 Internal server error.");
+    } // GETERROR()
+
+    // POST METHODS
     @PostMapping("/crops/new")
     public Crop createCrop(@Valid @RequestBody Crop crop) {
         return cropRepository.save(crop);
     } // CREATECROP(crop)
 
+    // PUT METHODS
     @PutMapping("/crops/{id}")
     public ResponseEntity<Crop> updateCrop(
             @PathVariable(value = "id") Long cropId, @Valid @RequestBody Crop cropDetails)
@@ -56,6 +68,7 @@ public class CropController {
         return ResponseEntity.ok(updatedCrop);
     } // UPDATECROP(@PATHVARIABLE)
 
+    // DELETE METHODS
     @DeleteMapping("/crops/delete/{cropId}")
     public Map<String, Boolean> deleteCrop(@PathVariable(value = "cropId") Long cropId) throws Exception {
         Crop crop =
@@ -67,15 +80,5 @@ public class CropController {
         response.put("deleted", Boolean.TRUE);
         return response;
     } // DELETECROP(@PATHVARIABLE)
-
-    @GetMapping("/")
-    public ResponseEntity<String> getHome() throws Exception{
-        return ResponseEntity.ok("We are at home page.");
-    } // GETHOME()
-
-    @GetMapping("/error")
-    public ResponseEntity<String> getError() throws Exception{
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("500 Internal server error.");
-    } // GETERROR()
 
 } // CLASS
