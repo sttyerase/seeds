4bdd1d2ae86c50b6980615ed895b1b73a8d560d7
Daniel Moore <sttyerase@gmail.com>
2020-08-11 10:27:52 -0400

    Get rid of files that were not supposed to be committed.

--- src/test/scripts/api_test.sh
+++
@@ -1,7 +0,0 @@
-
-## VALIDATE ROOT PAGE
-curl http://localhost:8080/seedinspection/
-if [ $? -ne 0 ] ; then echo "FAILURE at home page."; else echo "SUCCESS" ; fi
-## VALIDATE GET ALL
-curl http://localhost:8080/seedinspection/crops/all
-if [ $? -ne 0 ] ; then echo "FAILURE at home page."; else echo "SUCCESS" ; fi
