4401ca06a1ea390343559e58d33d26fbb1e747f3
Daniel Moore <sttyerase@gmail.com>
2020-09-17 19:47:21 -0400

    Converted to JpaRepository from CrudRepository for advanced features.

--- src/main/java/com/dbmi/seeds/model/ProducerRepository.java
+++ src/main/java/com/dbmi/seeds/model/ProducerRepository.java
@@ -1,11 +1,11 @@
 package com.dbmi.seeds.model;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface ProducerRepository extends CrudRepository<Producer, Long> {
+public interface ProducerRepository extends JpaRepository<Producer, Long> {
     Optional<Producer> findByProducerShortName(String producerShortName);
 } // INTERFACE
--- src/main/java/com/dbmi/seeds/model/VarietyRepository.java
+++ src/main/java/com/dbmi/seeds/model/VarietyRepository.java
@@ -1,11 +1,11 @@
 package com.dbmi.seeds.model;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface VarietyRepository extends CrudRepository<Variety, Long> {
+public interface VarietyRepository extends JpaRepository<Variety, Long> {
     Optional<Variety> findByVarietyName(String varietyName);
 } // INTERFACE
